2493d28461d8aba3e0635e6f85b0b0c46981a6b2
Jason Noel <jason.noel.imp@gmail.com>
2019-06-22 15:17:18 +0200

    Flash notice / errors + article/show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,22 @@
 class ArticlesController < ApplicationController
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
         @article = Article.new
     end
 
     def create
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        if @article.save
+            flash[:notice] = "l'article bel et bien créé !"
+            redirect_to article_path(@article)
+        else
+            #flash[:notice] = @article.errors.full_messages
+            render 'new'
+        end
     end
 
     private
