2c875ca7ad8771e84670a008cd85c6e55c05b985
Jason Noel <jason.noel.imp@gmail.com>
2019-06-26 16:37:01 +0200

    login form front created

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
   resources :users, except: [:new]
+  get 'signup', to: 'users#new', as: 'new_user'
 
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  get 'signup', to: 'users#new', as: 'new_user'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 end
