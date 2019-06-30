231af0cad6cd03ffa0f313f072c67c3b01da162e
Jason Noel <jason.noel.imp@gmail.com>
2019-06-30 12:16:32 +0200

    connexion on signup + better redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Votre compte a bien été créé #{@user.username} !"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'new'
         end
@@ -31,7 +32,7 @@ class UsersController < ApplicationController
     def update
         if @user.update(user_params)
             flash[:success] = "Votre compte est bel et bien modifié #{@user.username} !"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'edit'
         end
